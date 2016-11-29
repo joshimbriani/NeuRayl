3d1f38262375e0a5a79fb7ae6ae247bf01b91479
Josh Imbriani <joshimbriani@gmail.com>
2016-11-28 22:21:58 -0600

    Converted to Python 3 -- for Tensorflow

--- processor.py
+++ processor.py
@@ -2,29 +2,29 @@ from __future__ import division
 import csv
 
 def getBattersData():
-	with open('data/Batting.csv', 'rb') as csvfile:
+	with open('data/Batting.csv', 'rt') as csvfile:
 		data = []
 		reader = csv.reader(csvfile)
-		reader.next()
+		next(reader, None)
 		for row in reader:
 			if '' not in row:
 				data.append([row[0], int(row[1]), int(row[2]), row[3], row[4], int(row[5]), int(row[6]), int(row[7]), int(row[8]), int(row[9]), int(row[10]), int(row[11]), int(row[12]), int(row[13]), int(row[14]), int(row[15]), int(row[16]), int(row[17]), int(row[18]), int(row[19]), int(row[20]), int(row[21])])
 	return data
 
 def getPitchersData():
-	with open('data/Pitching.csv', 'rb') as csvfile:
+	with open('data/Pitching.csv', 'rt') as csvfile:
 		reader = csv.reader(csvfile)
 		data = []
-		reader.next()
+		next(reader, None)
 		for row in reader:
 			data.append(row)
 	return data
 
 def loadPlayerInfo():
-	with open('data/Master.csv', 'rb') as csvfile:
+	with open('data/Master.csv', 'rt') as csvfile:
 		reader = csv.reader(csvfile)
 		data = {}
-		reader.next()
+		next(reader, None)
 		for row in reader:
 			temp = list(row[1:])
 			temp.append([])
@@ -33,11 +33,11 @@ def loadPlayerInfo():
 	return data
 
 def getTeamData():
-	with open('data/Teams.csv', 'rb') as csvfile:
+	with open('data/Teams.csv', 'rt') as csvfile:
 		reader = csv.reader(csvfile)
 		data = {}
 		leagueAdjust = {}
-		reader.next()
+		next(reader, None)
 		for row in reader:
 			if row[2] not in data:
 				data[row[2]] = {}
@@ -46,16 +46,16 @@ def getTeamData():
 				leagueAdjust[int(row[0])] = []
 			leagueAdjust[int(row[0])].append(int(row[14]))
 
-	for year, values in leagueAdjust.iteritems():
+	for year, values in leagueAdjust.items():
 		leagueAdjust[year] = sum(values) / len(values)
 		
 	avg = 0
-	for year, values in leagueAdjust.iteritems():
+	for year, values in leagueAdjust.items():
 		avg += values
 
 	avg /= len(leagueAdjust)
 	
-	for year, values in leagueAdjust.iteritems():
+	for year, values in leagueAdjust.items():
 		leagueAdjust[year] = 2 - (leagueAdjust[year] / avg)
 
 	return (data, leagueAdjust)
@@ -70,7 +70,7 @@ def combineYearsByKey(years):
 			for i in range(6, len(year)):
 				runningYears[year[2]][i] += year[i]
 	
-	for key, value in runningYears.iteritems():
+	for key, value in runningYears.items():
 		finalYears.append(value)
 
 	return finalYears
